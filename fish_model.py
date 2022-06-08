432571d80e5fc8e26857b4d8133ebbd6dfb5e3e4
Drysiek <505rysiek@gmail.com>
2022-06-08 20:56:31 +0200

    better placement of code
    better placement of code, better selection of data, different way of creating figure

--- fish_model.py
+++ fish_model.py
@@ -3,24 +3,30 @@ import os
 
 
 def get_data():
-    if not os.path.isfile('random_fish_stocking_rows.csv'):
+    file = 'random_fish_stocking_rows.csv'
+    if not os.path.isfile(file):
         print('Downlaoding data from: ', 'Fish Stocking.csv')
         data_frame = pd.read_csv('Fish Stocking.csv', sep=',')
         data_frame = data_frame[data_frame['Year'].notnull()]
-        data_frame = data_frame[data_frame['County'].notnull()]
         data_frame = data_frame[data_frame['Waterbody'].notnull()]
-        data_frame = data_frame[data_frame['Town'].notnull()]
         data_frame = data_frame[data_frame['Month'].notnull()]
         data_frame = data_frame[data_frame['Number'].notnull()]
+        data_frame = data_frame[data_frame['Number'] < 10000]  # this kind of data will look better on the figure
         data_frame = data_frame[data_frame['Species'].notnull()]
         data_frame = data_frame[data_frame['Size'].notnull()]
-        data_frame = data_frame.sample(n=2000)
-        data_frame.to_csv('random_fish_stocking_rows.csv', index=False)
+        data_frame = data_frame.drop(['County'], axis=1)
+        data_frame = data_frame.drop(['Town'], axis=1)
+
+        bool_series = data_frame.duplicated(['Year', 'Waterbody', 'Month', 'Species', 'Size'], keep=False)
+        data_frame = data_frame[~bool_series]
+
+        data_frame = data_frame.sample(n=600)
+        data_frame.to_csv(file, index=False, sep=',')
         print('Downloaded')
     else:
-        print(f'File "random_fish_stocking_rows.csv" already exists')
+        print(f'File {file} already exists')
 
-    x = pd.read_csv('random_fish_stocking_rows.csv', sep=',')
+    x = pd.read_csv(file, sep=',')
     y = x['Number']
     x = x.drop(['Number'], axis=1)
 
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fish_model import get_data, func, CustomModelWrapper
 from scipy.optimize import curve_fit
 import numpy as np
-import seaborn as sns
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
@@ -9,40 +8,60 @@ from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
 
 
+def linear_model(x_train, x_test, y_train, y_test):
+    model_lin = LinearRegression()
+    model_lin.fit(x_train, y_train)
+    y_predicted_lin = model_lin.predict(x_test)
+    print("\nMean absolute percentage error from linearRegression:")
+    print(mean_absolute_percentage_error(y_test, y_predicted_lin))
+    print("Mean squared error from linearRegression:")
+    print(mean_squared_error(y_test, y_predicted_lin))
+    return y_predicted_lin
+
+
+def SVR_model(x_train, x_test, y_train, y_test):
+    model_svr = SVR()
+    model_svr.fit(x_train, y_train)
+    y_predicted_svr = model_svr.predict(x_test)
+    print("\nMean absolute percentage error from SVR")
+    print(mean_absolute_percentage_error(y_test, y_predicted_svr))
+    print("Mean squared error from SVR")
+    print(mean_squared_error(y_test, y_predicted_svr))
+    return y_predicted_svr
+
+
+def custom_model(x_train, x_test, y_train, y_test):
+    parameters, _ = curve_fit(func, xdata=x_train, ydata=y_train.values.ravel(), p0=np.ones(len(x_train.columns) + 1))
+    model_custom = CustomModelWrapper(func, parameters)
+    y_predicted_custom = model_custom.predict(x_test)
+    print("\nMean absolute percentage error from curve_fit")
+    print(mean_absolute_percentage_error(y_test, y_predicted_custom))
+    print("Mean squared error from curve_fit")
+    print(mean_squared_error(y_test, y_predicted_custom))
+    return y_predicted_custom
+
+
 if __name__ == '__main__':
-    X, Y = get_data()
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.20, random_state=40)
+    x, y = get_data()
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.20, random_state=40)
 
     # Linear model
-    model_lin = LinearRegression()
-    model_lin.fit(X_train, Y_train)
-    Y_predicted_lin = model_lin.predict(X_test)
-    print("\nMean absolute percentage error from linearRegression:")
-    print(mean_absolute_percentage_error(Y_test, Y_predicted_lin))
-    print("Mean squared error from linearRegression:")
-    print(mean_squared_error(Y_test, Y_predicted_lin))
+    y_predicted_l = linear_model(x_train, x_test, y_train, y_test)
 
     # Model SVR(Support Vector Regression)
-    model_svr = SVR()
-    model_svr.fit(X_train, Y_train)
-    Y_predicted_svr = model_svr.predict(X_test)
-    print("\nMean absolute percentage error from SVR")
-    print(mean_absolute_percentage_error(Y_test, Y_predicted_svr))
-    print("Mean squared error from SVR")
-    print(mean_squared_error(Y_test, Y_predicted_svr))
+    y_predicted_s = SVR_model(x_train, x_test, y_train, y_test)
 
-    parameters, _ = curve_fit(func, xdata=X_train, ydata=Y_train.values.ravel(), p0=np.ones(len(X_train.columns)+1))
-    model_custom = CustomModelWrapper(func, parameters)
-    Y_predicted_custom = model_custom.predict(X_test)
-    print("\nMean absolute percentage error from curve_fit")
-    print(mean_absolute_percentage_error(Y_test, Y_predicted_custom))
-    print("Mean squared error from curve_fit")
-    print(mean_squared_error(Y_test, Y_predicted_custom))
+    # Custom model
+    y_predicted_c = custom_model(x_train, x_test, y_train, y_test)
 
-    reach = int(len(Y_test))
+    reach = range(0, int(len(y_test)))
 
-    sns.scatterplot(x=range(0, reach), y=Y_test, color="white", edgecolor="black")
-    sns.lineplot(x=range(0, reach), y=Y_predicted_lin, color="red")
-    sns.lineplot(x=range(0, reach), y=Y_predicted_svr, color="green")
-    sns.lineplot(x=range(0, reach), y=Y_predicted_custom, color="orange")
+    plt.title('Regressions and actual values')
+    plt.xlabel("Test number")
+    plt.ylabel("Number of fish")
+
+    plt.scatter(reach, y_test)
+    plt.plot(reach, y_predicted_l, 'b', label='linear regression')
+    plt.plot(reach, y_predicted_s, 'c', label='SVR model regression')
+    plt.plot(reach, y_predicted_c, 'r', label='custom regression')
     plt.show()
